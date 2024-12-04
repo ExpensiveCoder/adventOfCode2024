055ea856429974da10d70ce20f63292888866b9a
Joshua McD <77360758+ExpensiveCoder@users.noreply.github.com>
2024-12-04 15:30:15 -0600

    Update day1.py

--- day1.py
+++ day1.py
@@ -2,14 +2,31 @@
 # Day 1: Historian Hysteria
 # Part 1
 
-## Read file
+list1 = []
+list2 = []
+distancelist = []
+
+# Read file
 file = open("data/day1.txt", "r")
 
-## TODO: Put Data into 2 Lists (list1 and list2)
+# Function to find distance between 2 numbers
+def disCalc(num1, num2):
+    if(num1 > num2):
+        distance = num1 - num2
+    else:
+        distance = num2 - num1
+    return distance
 
-## TODO: Sort lists from smallest to largest
+# Put Data into 2 Lists (list1 and list2)
+for line in file:
+    parts = line.split()
+    list1.append([int(parts[0])])
+    list2.append([int(parts[1])])
+# Sort lists from smallest to largest
+list1.sort()
+list2.sort()
+# TODO: Find distance between each list input (larger - smaller = distance)
 
-## TODO: Find distance between each list input (larger - smaller = distance)
 ## TODO: Put distances into new list
 
 ## TODO: Find and print Sum of distances list
