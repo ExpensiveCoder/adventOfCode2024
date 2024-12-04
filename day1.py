66f85b869fe892717b535d4daa500c970fcfd2f6
Joshua McD <77360758+ExpensiveCoder@users.noreply.github.com>
2024-12-04 14:17:45 -0600

    Update day1.py

--- day1.py
+++ day1.py
@@ -1,4 +1,18 @@
 # Advent of Code 2024
 # Day 1: Historian Hysteria
 # Part 1
-  
+
+## Read file
+file = open("data/day1.txt", "r")
+
+## TODO: Put Data into 2 Lists (list1 and list2)
+
+## TODO: Sort lists from smallest to largest
+
+## TODO: Find distance between each list input (larger - smaller = distance)
+## TODO: Put distances into new list
+
+## TODO: Find and print Sum of distances list
+
+
+# Part 2
