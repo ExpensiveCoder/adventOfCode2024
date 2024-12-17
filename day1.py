bbf2bdb546c899785669518363fd7ee7ba8f5dfe
Joshua McD <77360758+ExpensiveCoder@users.noreply.github.com>
2024-12-17 15:32:24 -0600

    Update day1.py

--- day1.py
+++ day1.py
@@ -4,8 +4,8 @@
 
 list1 = []
 list2 = []
-distList = []
-count = 0
+distancelist = []
+dis = 0
 
 # Read file
 file = open("data/day1.txt", "r")
@@ -20,22 +20,17 @@ def disCalc(num1, num2):
 
 # Put Data into 2 Lists (list1 and list2)
 for line in file:
-    parts = line.split()
-    list1.append([int(parts[0])])
-    list2.append([int(parts[1])])
-    count += 1
+    list1.append(int(line.split()[0].strip()))
+    list2.append(int(line.split()[1].strip()))
 # Sort lists from smallest to largest
 list1.sort()
 list2.sort()
-# TODO: Find distance between each list input (larger - smaller = distance)
-for i in count:
-    dist = distCalc(list1[i], list2[i])
-    ## TODO: Put distances into new list
-    distList.append(dist)
+# Find distance between each list input (larger - smaller = distance)
+for i in range(0, len(list1)):
+    dis += abs(list2[i] - list1[i])
 
-## TODO: Find and print Sum of distances list
-sum = sum(distList)
-print(f"The total distance between lists is ", sum)
+# Print Sum of distances list
 
+print(f"The sum of the distances is ", dis)
 
 # Part 2
